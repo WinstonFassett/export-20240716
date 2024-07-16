6cbf8ae64724ec59d26192442f01c1db342846d5
Winston Fassett <winston.fassett@gmail.com>
2024-07-16 17:03:55 -0500

    ❯ npx shadcn-ui@latest init ✔ Would you like to use TypeScript (recommended)? … no / yes ✔ Which style would you like to use? › Default ✔ Which color would you like to use as base color? › Slate ✔ Where is your global CSS file? … src/ui/global.css ✔ Would you like to use CSS variables for colors? … no / yes ✔ Are you using a custom tailwind prefix eg. tw-? (Leave blank if not) … ✔ Where is your tailwind.config.js located? … tailwind.config.js ✔ Configure the import alias for components: … @/components ✔ Configure the import alias for utils: … @/lib/utils ✔ Are you using React Server Components? … no / yes ✔ Write configuration to components.json. Proceed? … yes
    ✔ Writing components.json...
    ✔ Initializing project...
    ✔ Installing dependencies...
    
    Success! Project initialization completed. You may now add components

--- vite.config.app.ts
+++ vite.config.app.ts
@@ -4,17 +4,14 @@ import baseConfig from './vite.config.base.js';
 
 export default defineConfig({
   ...baseConfig,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   root: path.resolve(__dirname, 'demo'),
   base: '/',
   build: {
     outDir: '../dist/app',
   },
-  compilerOptions: {
-    "baseUrl": ".",
-    "paths": {
-      "@/*": [
-        "./src/*"
-      ]
-    }    
-  }
 });
