75dc117f7610a799eec6f6a6010eb4afdd25f534
Winston Fassett <winston.fassett@gmail.com>
2024-07-16 16:27:00 -0500

    Restore correct build and chakra provider

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,13 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { ConfirmDelivery } from "../../src/stories/ConfirmDelivery";
 import { PurchaseSteps } from "../../src/stories/PurchaseSteps";
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <PurchaseSteps />
       <ConfirmDelivery />
-    </>
+    </ChakraProvider>
   );
 }
 
--- vite.config.base.ts
+++ vite.config.base.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {    
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime', 'react-feather', 'framer-motion'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'react/jsx-runtime',
-          'react-feather': 'ReactFeather',
-          'framer-motion': 'FramerMotion'          
-        },
-      },
-    },
-  },
+  plugins: [react()],  
 })
