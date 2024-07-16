35c161f9c7d567df43220101e49ec84d9ca3de51
Winston Fassett <winston.fassett@gmail.com>
2024-07-16 16:47:06 -0500

    oops restoring this after overzealous cleanup because some Doc pages depended on it

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     "@chakra-ui/storybook-addon",
     "storybook-addon-pseudo-states"
   ],
